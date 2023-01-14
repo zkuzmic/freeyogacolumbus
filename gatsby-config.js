921003cd8c1abe02c401764c4519cc903417fe28
Zach Kuzmic <zkuzmic@indeed.com>
2023-01-14 08:55:26 -0600

    change site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Free Yoga Columbus',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Brought to you by Darlene Kuzmic',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
